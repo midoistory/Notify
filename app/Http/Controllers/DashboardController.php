5a6077a4c08edfba74f5044c4e8bc11d00f85375
midoistory <midoriharahap483@gmail.com>
2024-11-18 11:26:00 +0700

    feat: task count

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Subject;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -10,7 +11,8 @@ class DashboardController extends Controller
     public function index()
     {
         $subjectCount = Subject::count();
+        $taskCount = Task::count();
 
-        return view('admin.dashboard', compact('subjectCount'));
+        return view('admin.dashboard', compact('subjectCount', 'taskCount'));
     }
 }
